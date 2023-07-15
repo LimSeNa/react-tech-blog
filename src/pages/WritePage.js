9d586ab9b722d722c1fd347258bdb76bcc1966ca
Sena Lim <tpsk8322@naver.com>
2023-07-16 05:46:18 +0900

    글쓰기 페이지 UI 완성

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,6 +1,15 @@
+import Editor from "../components/write/Editor";
+import Responsive from "../components/common/Responsive";
+import TagBox from "../components/write/TagBox";
+import WriteActionButtons from "../components/write/WriteActionButtons";
+
 const WritePage = () => {
     return (
-        <div>글쓰기</div>
+        <Responsive>
+            <Editor/>
+            <TagBox/>
+            <WriteActionButtons/>
+        </Responsive>
     );
 };
 
